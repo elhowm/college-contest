e44ab849bf482c5f88739daeb448f6028f81d456
safych <v7lad@ukr.net>
2021-10-05 14:30:50 +0300

    Create class Student. Initialize class Student.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,1 +1,10 @@
-class Student; end
+class Student
+  attr_accessor :name, :surname, :age
+
+  def initialize (name, surname, age)
+      @name = name
+      @surname = surname
+      @age = age
+  end
+
+end
