191870b36191ccbe099fb2b7f5b54e864592b195
safych <v7lad@ukr.net>
2021-10-06 11:47:45 +0300

    Bug fixes

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,1 +1,11 @@
-class Group; end
+class Group
+
+  attr_accessor :capacity
+
+  def initialize (capacity)
+    @capacity = capacity
+  end
+
+end
+
+student = new Student('Alex', 'Prysyazhnyi', 18)
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,11 @@
 class Student
-  attr_accessor :name, :surname, :age
+  attr_accessor :name,
+                :surname,
+                :age
 
-  def initialize (name, surname, age)
-      @name = name
-      @surname = surname
-      @age = age
+  def initialize(name, surname, age)
+    @name = name
+    @surname = surname
+    @age = age
   end
-
 end
