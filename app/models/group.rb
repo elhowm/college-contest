1a10dbddab10048de8f16e859e141fb950276bb4
safych <v7lad@ukr.net>
2021-10-14 20:10:45 +0300

    Create and initialize class Group. Creating a class group of array students attribute.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,1 +1,16 @@
-class Group; end
+require_relative 'student'
+
+class Group
+  attr_accessor :students
+
+  def initialize(capacity)
+    @students = []
+    for a in 1..capacity do
+      surname = "surname " + a.to_s
+      name = "name " + a.to_s
+      age = rand(17..24)
+      student = Student.new(name, surname, age)
+      @students.push(student)
+    end
+  end
+end
