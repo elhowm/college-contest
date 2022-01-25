4a36cea208d5896b0a0919fc9114e8dd6ba4bd9a
safych <v7lad@ukr.net>
2022-01-25 16:59:18 +0200

    Fixed sorting

--- app/models/group.rb
+++ app/models/group.rb
@@ -15,32 +15,26 @@ class Group
       student = Student.new(name, surname, age)
       @students.push(student)
     end
-    @flag = true
   end
 
   def info
     list = "      Group name: \"#{@name}\"\n\n"
     list += "      Students:\n"
-    @students.sort! { |a, b|  a.name <=> b.name }
+    @students.sort_by { |student| [student.name] }
     .each { |student| list += "      - #{student.name}\n" }
     list
   end
 
   def search(sr)
-    @students.sort! { |a, b|  a.surname <=> b.surname }
-    list = @students.select { |e| e.name.include?(sr) }
-    @students.sort! { |a, b|  a.name <=> b.name }
-    list = @students.select { |e| e.name.include?(sr) or e.surname.include?(sr) }
-    list = @students.select { |e| "#{e.name} #{e.surname}".include?(sr) }
-    if @flag == true
-      list
-    else
-      list.first
-    end
+    list = @students.sort_by { |student| [student.name, student.surname] }
+                    .select { |e| e.name.include?(sr) }
+    list = @students.sort_by { |student| [student.name, student.surname] }
+                    .select { |e| e.name.include?(sr) or e.surname.include?(sr) or
+                    "#{e.name} #{e.surname}".include?(sr) }
+    list
   end
 
   def find(sr)
-    @flag = false
-    search(sr)
+    search(sr).first
   end
 end
