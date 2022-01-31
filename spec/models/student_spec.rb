f4ca93da9b5bb5f59f0d2ad46b2385adbe7ce271
safych <v7lad@ukr.net>
2022-01-31 10:32:53 +0200

    Added test for method full_name

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,4 +1,5 @@
 require 'app_helper'
+require 'models/group_spec'
 
 describe Student do
   subject(:student) { Student.new(name, surname, age) }
@@ -10,4 +11,18 @@ describe Student do
   it { expect(student.name).to eq name }
   it { expect(student.surname).to eq surname }
   it { expect(student.age).to eq age }
+
+  describe '#full_name' do
+    let(:kris) { Student.new('Kris', 'Albarn', 18) }
+    let(:bob) { Student.new('Bob', 'Dilan', 19) }
+    let(:alan_wake) { Student.new('Alan', 'Wake', 20) }
+    let(:alan_rosbeef) { Student.new('Alan', 'Rosbeef', 20) }
+
+    it 'returns full name' do
+      expect(kris.full_name).to eq 'Kris Albarn'
+      expect(bob.full_name).to eq 'Bob Dilan'
+      expect(alan_wake.full_name).to eq 'Alan Wake'
+      expect(alan_rosbeef.full_name).to eq 'Alan Rosbeef'
+    end
+  end
 end
